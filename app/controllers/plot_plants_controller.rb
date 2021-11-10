d8b00dc0f6038e4032922a85fba37967530c3a7f
Haewon Jeon <haewonito@gmail.com>
2021-11-10 12:12:08 -0700

    US2 complete

--- app/controllers/plot_plants_controller.rb
+++ app/controllers/plot_plants_controller.rb
@@ -2,8 +2,8 @@ class PlotPlantsController < ApplicationController
 
   def destroy
     plot = Plot.find(params[:plot_id_to_delete])
-    plant = Plant.find(name: params[:plant_name_to_delete])
-    plot_plant = PlotPlant.find(plot_id: plot.id, plant_id: plant.id)
+    plant = Plant.find_by(name: params[:plant_name_to_delete])
+    plot_plant = PlotPlant.find_by(plot_id: plot.id, plant_id: plant.id)
 
     plot_plant.destroy
 
